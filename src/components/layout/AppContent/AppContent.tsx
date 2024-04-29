2092db327c7cc3b1415f220c6c7c62a7174d912e
Dmitrii <stepanov.dmitrii.spo@gmail.com>
2024-04-29 21:43:50 +0400

    4th commiut

--- src/components/layout/AppContent/AppContent.tsx
+++ src/components/layout/AppContent/AppContent.tsx
@@ -2,5 +2,5 @@ import { Content } from 'antd/es/layout/layout'
 import styles from './AppContent.module.scss'
 
 export default function AppContent() {
-	return <Content className={styles.contentStyle}>Content</Content>
+	return <Content className={styles.contentStyle}>Footer</Content>
 }
